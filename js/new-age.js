1e577c2bc4314b65b64ae67be71dafec01caed1f
rjmcbain <rmcbain01@gmail.com>
2018-05-15 16:07:46 -0600

    adding contact form and firebase

--- js/new-age.js
+++ js/new-age.js
@@ -3,26 +3,34 @@
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 48
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
+  $("body").scrollspy({
+    target: "#mainNav",
     offset: 54
   });
 
@@ -38,5 +46,46 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
 })(jQuery); // End of use strict
+
+// Listen for form submit
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBezI0s1Au28QTw0FWUrNbn8uYcB-iqMok",
+  authDomain: "contactform-bc5c0.firebaseapp.com",
+  databaseURL: "https://contactform-bc5c0.firebaseio.com",
+  projectId: "contactform-bc5c0",
+  storageBucket: "contactform-bc5c0.appspot.com",
+  messagingSenderId: "104228030019"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+let messagesRef = firebase.database().ref("messages");
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  // Get Values
+  let email = getInputVal("email");
+
+  console.log(email);
+}
+
+// Function to get form values
+
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+
+function saveMessage(email) {
+  let newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    email: email
+  });
+}
